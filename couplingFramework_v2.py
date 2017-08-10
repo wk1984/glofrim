fab2591d1474402ad3ab8d87ff6195879fade292
ChippChapp <j.m.hoch@uu.nl>
2017-08-10 15:09:02 +0200

    further changes in cF_v2.py
    shlighly reshuffled strucutre; added some clearer conditional structures; merged inundation threshold for river and floodplain into one;

--- couplingFramework_v2.py
+++ couplingFramework_v2.py
@@ -118,9 +118,7 @@ secPerDay                             = 86400.
 end_time 							  = nr_pcr_timesteps * secPerDay
 fraction_timestep 					  = secPerDay / update_step
 
-threshold_inundated_depth             = float(config.numerical_settings['threshold_inundated_depth'])                         
-threshold_inundated_depth_rivers      = float(config.numerical_settings['threshold_inundated_depth_rivers'])                         
-threshold_inundated_depth_floodplains = float(config.numerical_settings['threshold_inundated_depth_floodplains'])                          
+threshold_inundated_depth             = float(config.numerical_settings['threshold_inundated_depth'])                                                   
 
 # other
 missing_value_landmask                = 255
@@ -233,23 +231,26 @@ elif use_2way == True:
 
 # saving plots of coupled cells to verbose-folder
 # currently doesn't work with FM and use_RFS on, due to data structure required (? check this ?)
-if (verbose == True) and (use_2way == False):
-    coupling_functions.plotGridfromCoords(PCRcoords, modelCoords)
-    plt.savefig(os.path.join(verbose_folder , 'AllCells_1way.png'))
-    coupling_functions.plotGridfromCoords(CoupledCellsInfoAll[1],CoupledCellsInfoAll[0])
-    plt.savefig(os.path.join(verbose_folder , 'CoupledCells_1way.png'))   
-    plt.close('all')
-elif (verbose == True) and (use_2way = True):
-    coupling_functions.plotGridfromCoords(PCRcoords, modelCoords)
-    plt.savefig(os.path.join(verbose_folder , 'AllCells_1way.png'))
-    coupling_functions.plotGridfromCoords(CoupledCellsInfoAll[1],CoupledCellsInfoAll[0])
-    plt.savefig(os.path.join(verbose_folder , 'CoupledCells_1way.png'))   
-    plt.close('all')    
-    coupling_functions.plotGridfromCoords(PCRcoords, modelCoords_2way)
-    plt.savefig(os.path.join(verbose_folder , 'AllCells_2way.png'))
-    coupling_functions.plotGridfromCoords(CoupledCellsInfoAll_2way[1],CoupledCellsInfoAll_2way[0])
-    plt.savefig(os.path.join(verbose_folder , 'CoupledCells_2way.png'))   
-    plt.close('all')
+if (model_type == 'DFM') and (use_RFS == True):
+    pass
+elif:
+	if (verbose == True) and (use_2way == False):
+		coupling_functions.plotGridfromCoords(PCRcoords, modelCoords)
+		plt.savefig(os.path.join(verbose_folder , 'AllCells_1way.png'))
+		coupling_functions.plotGridfromCoords(CoupledCellsInfoAll[1],CoupledCellsInfoAll[0])
+		plt.savefig(os.path.join(verbose_folder , 'CoupledCells_1way.png'))   
+		plt.close('all')
+	elif (verbose == True) and (use_2way = True):
+		coupling_functions.plotGridfromCoords(PCRcoords, modelCoords)
+		plt.savefig(os.path.join(verbose_folder , 'AllCells_1way.png'))
+		coupling_functions.plotGridfromCoords(CoupledCellsInfoAll[1],CoupledCellsInfoAll[0])
+		plt.savefig(os.path.join(verbose_folder , 'CoupledCells_1way.png'))   
+		plt.close('all')    
+		coupling_functions.plotGridfromCoords(PCRcoords, modelCoords_2way)
+		plt.savefig(os.path.join(verbose_folder , 'AllCells_2way.png'))
+		coupling_functions.plotGridfromCoords(CoupledCellsInfoAll_2way[1],CoupledCellsInfoAll_2way[0])
+		plt.savefig(os.path.join(verbose_folder , 'CoupledCells_2way.png'))   
+		plt.close('all')
     
 # -------------------------------------------------------------------------------------------------
 # TURNING OFF CHANNELSTORAGE, WATERBODYSTORAGE, WATERBODIES AND RUNOFF TO CHANNELS
@@ -258,15 +259,29 @@ elif (verbose == True) and (use_2way = True):
 model_functions.noStorage(model_pcr, missing_value_pcr, CoupledPCRcellIndices, CouplePCR2model)
 
 # -------------------------------------------------------------------------------------------------
+# TURNING OFF ROUTING BY PCR IN COUPLED AREA
+# -------------------------------------------------------------------------------------------------
+
+model_functions.noLDD(model_pcr, CoupledPCRcellIndices, verbose_folder, verbose)
+
+# -------------------------------------------------------------------------------------------------
 # ACTIVATING A RANGE OF VARIABLES SPECIFICALLY REQUIRED FOR 2WAY-COUPLING
 # -------------------------------------------------------------------------------------------------
 
 new_preventRunoffToDischarge, new_controlDynamicFracWat, new_waterBodyIdsAdjust \ 
          = model_functions.activate2wayVariables(model_pcr, CoupledPCRcellIndices, CouplePCR2model)
+		 
+# -------------------------------------------------------------------------------------------------
+# DETERMINING INUNDATED AREA IN HYDRODYNAMIC MODEL AND ITS FRACTION WITH PCR-CELL
+# -------------------------------------------------------------------------------------------------
          
 inundated_area_FM_2_PCR_coupled, inundated_fraction_FM_2_PCR =  
-    determine_InundationArea_Hydrodynamics(CouplePCR2model_2way, CoupledPCRcellIndices_2way, threshold_inundated_depth_floodplains, cellAreaSpherical, cellarea_data_pcr)
-         
+    determine_InundationArea_Hydrodynamics(CouplePCR2model_2way, CoupledPCRcellIndices_2way, threshold_inundated_depth, cellAreaSpherical, cellarea_data_pcr)
+
+# -------------------------------------------------------------------------------------------------
+# DETERMINING INUNDATION DEPTH IN HYDRODYNAMIC MODEL
+# -------------------------------------------------------------------------------------------------
+	
 water_depths_FM_2_PCR = determine_InundationDepth_Hydrodynamics(model_hydr, landmask_pcr, inundated_area_FM_2_PCR_coupled, CoupledPCRcellIndices_2way, CouplePCR2model_2way)
 
 # -------------------------------------------------------------------------------------------------
@@ -277,12 +292,6 @@ TO DO: get all required variables and how their are determined from Arjen's orig
 """
 #model_functions.updateHydrologicVariables(model_pcr, new_preventRunoffToDischarge, new_controlDynamicFracWat, new_waterBodyIdsAdjust, water_depths_FM_2_PCR, \
 #            inundated_fraction_FM_2_PCR, new_channelStorage_pcr, inundated_fraction_rivers_FM_2_PCR, new_controlFloodplainFactor, use_floodplain_infiltration_factor)
-
-# -------------------------------------------------------------------------------------------------
-# TURNING OFF ROUTING BY PCR IN COUPLED AREA
-# -------------------------------------------------------------------------------------------------
-
-model_functions.noLDD(model_pcr, CoupledPCRcellIndices, verbose_folder, verbose)
 
 # -------------------------------------------------------------------------------------------------
 # CALCULATE DELTA VOLUMES (DAY 1)
